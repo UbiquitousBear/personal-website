d37e25062cf19526a144a5eba06e06cf74f5da60
Shamil Nunhuck <shamil@shamil.co.uk>
2020-03-29 22:57:30 +0100

    Hotfix: Fixing icon sizes on the home page

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -45,7 +45,7 @@ class Home extends Component<HomeProps> {
 							<h3>Latest Posts <Button color="light" href="/blog" size="sm">View All</Button></h3>
 							{ blogItems.map((blogItem: BlogItem) => (
 								<Button className={ style.postButton } block={true} href={ this.buildBlogUrl(blogItem) }>
-									<img src={blogItem.icon.data} height="150" width="150" />
+									<img src={blogItem.icon.data} height="20px" width="20px" />  • 
 									{ blogItem.title }
 									{ this.renderNewBadge(blogItem) }
 								</Button>
